9a1f36ea60d5fbb1c9fd0e144a563640eeaa65ef
fpnav <fpnav@yahoo.com.br>
2016-05-02 18:32:17 -0300

    passando model e melhorando com tabelas bootstrap

---
+++ MvcTestGit/Controllers/ProductController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcTestGit.Models;
+
+namespace MvcTestGit.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var prod1 = new Product { Id = 1, Category = "Hardware", Name = "Keyboard", Price = 100};
+            var prod2 = new Product { Id = 2, Category = "Hardware", Name = "Mouse", Price = 50 };
+            var prod3 = new Product { Id = 3, Category = "Software", Name = "Antivirus", Price = 10 };
+            var Produtos = new List<Product>{prod1, prod2, prod3};
+
+            return View(Produtos);
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
